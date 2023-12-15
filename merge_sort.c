d7dabe551201d6aef75e067e670e0db6dbd68bbc
GANARAJU KARTHIK VARMA <114302009+KarthikVarma19@users.noreply.github.com>
2023-12-15 13:34:39 +0530

    Update merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -1,1 +1,34 @@
+/*merge sort */
+#include<stdio.h>
+void main() {
+int n,temp,i,j,num[50];
+  printf("\n Enter the no of elements");
+  scanf("%d",&n);
+  printf("Enter %d elements :",n);
+  for(i=0;i<n;i++) {
+    scanf("%d",&num[i]);
+  }
+  for(i=0;i<n;i++) {
+  if(num[i] > num[i+1]) {
+  temp = num[i];
+    num[j] = num[i];
+    num[i] = temp;
+  }
+  }
+  printf("the sorted array is : ");
+  for(i=0;i<n;i++) {
+  printf("%d",num[i]);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
